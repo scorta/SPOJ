81ff4c42b2aaa54dcdcfa025a1ee25bb121b074d
scorta <scorta@users.noreply.github.com>
2017-06-15 18:49:27 +0700

    Create UCV2013H_Slick.cpp

--- UCV2013H_Slick.cpp
+++ UCV2013H_Slick.cpp
@@ -1,83 +1,108 @@
-#include <iostream>
-#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+const int NMAX = 255;
+
+typedef pair<int, int> pt;
 
 int n, m;
-int count;
-std::vector<std::vector<int>> map;
-std::vector<std::vector<bool>> is_checked;
+
+const int dx[] = { 1, -1, 0, 0 };
+const int dy[] = { 0, 0, 1, -1 };
+
+int grid[NMAX][NMAX];
+//map<int, int> answer;
+map<int, int> answer;
 
 void print_matrix(){
 	printf("\n");
 	printf("\n");
-	for (int iN = 0; iN <= n + 1; iN++){
-		for (int iM = 0; iM <= m + 1; iM++){
-			printf("%*d", 2, map[iN][iM]);
+	for (int iN = 0; iN < n; iN++){
+		for (int iM = 0; iM < m; iM++){
+			printf("%*d", 2, grid[iN][iM]);
 		}
 		printf("\n");
 	}
 }
 
-void init(){
-	count = 2;
-	scanf("%d", &n);
-	scanf("%d", &m);
-
-	std::vector<std::vector<int>> temp_map(n + 2);
-	std::vector<std::vector<bool>> temp_is_checked(n + 2);
-	map = temp_map;
-	is_checked = temp_is_checked;
-
-	for (int iN = 0; iN <= n + 1; iN++){
-		for (int iM = 0; iM <= m + 1; iM++){
-			map[iN].push_back(-1);
-			is_checked[iN].push_back(false);
-		}
-	}
-
-	for (int iN = 1; iN <= n; iN++){
-		for (int iM = 1; iM <= m; iM++){
-			int temp;
-			scanf("%d", &temp);
-			map[iN][iM] = temp;
-		}
+void print_result(){
+	for (map<int, int>::iterator it = answer.begin(); it != answer.end(); it++){
+		printf("%d %d\n", *it);
 	}
 }
 
-void flood_fill(int row, int column, int mark){
-	is_checked[row][column] = true;
-	map[row][column] = mark;
-
-	if (map[row + 1][column] == 1 && !is_checked[row + 1][column]){
-		flood_fill(row + 1, column, mark);
-	}
-
-	if (map[row - 1][column] == 1 && !is_checked[row - 1][column]){
-		flood_fill(row - 1, column, mark);
-	}
-
-	if (map[row][column + 1] == 1 && !is_checked[row][column + 1]){
-		flood_fill(row, column + 1, mark);
-	}
-
-	if (map[row][column - 1] == 1 && !is_checked[row][column - 1]){
-		flood_fill(row, column - 1, mark);
-	}
+void Unmark(pt p) {
+	grid[p.first][p.second] = 2;
 }
 
-void BFS(){
-	for (int iN = 1; iN <= n; iN++){
-		for (int iM = 1; iM <= m; iM++){
-			if (map[iN][iM] == 1 && !is_checked[iN][iM]){
-				flood_fill(iN, iM, count++);
+void Bfs(pt start) {
+	int size = 0;
+	queue<pt> q;
+	q.push(start);
+	Unmark(start);
+
+	while (!q.empty()) {
+		pt v = q.front();
+		q.pop();
+
+		for (int dir = 0; dir < 4; ++dir) {
+			int next_x = v.first + dx[dir];
+			int next_y = v.second + dy[dir];
+
+			// check that we are inside grid
+
+			// check that new square is not visited
+
+			// unmark and push to the queue
+			if (next_x >= 0 && next_x < n && next_y >= 0 && next_y < m && grid[next_x][next_y] == 1){
+				pt newPoint;
+				newPoint.first = next_x;
+				newPoint.second = next_y;
+
+				Unmark(newPoint);
+				q.push(newPoint);
+				size++;
+			}
+			
+		}
+	}
+	answer[++size]++;
+	//printf("size %d", size);
+}
+
+void Solve(int n, int m) {
+	// goes through the grid and starts Bfs in some places
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++){
+			if (grid[i][j] == 1){
+				pt newPoint;
+				newPoint.first = i;
+				newPoint.second = j;
+				Bfs(newPoint);
 			}
 		}
-	}
 }
 
-int main(){
-	init();
-	BFS();
-	print_matrix();
+int main() {	
+	while (true) {
+		scanf("%d%d", &n, &m);
+
+		if (n == 0 || m == 0) break;
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				scanf("%d", &grid[i][j]);
+			}
+		}
+
+		Solve(n, m);
+		printf("\n\n");
+		print_result();
+	}
 
 	return 0;
 }
